8e78a11e607ade2bc71cb5caab10507a9e929015
LP-Ramos <ognemalf2@gmail.com>
2022-07-11 21:20:14 -0300

    CSS resposivo ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,32 @@ document.addEventListener('scroll', () => {
         console.log('sa')
     }
 });
+
+// 
+const nav = document.querySelector(".cabecalho__navegacao");
+
+const hoverOn = () => {
+    nav.classList.remove('nav-scroll-opacity');
+}
+
+const hoverOff = () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop >= 10) {
+        nav.classList.add('nav-scroll-opacity');
+    }
+}
+
+const onScroll = () => {
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    if (scrollTop >= 10) {
+        nav.classList.add("nav-scroll");
+        nav.classList.add("nav-scroll-opacity");
+    }else{
+        nav.classList.remove("nav-scroll");
+        nav.classList.remove('nav-scroll-opacity');
+    }
+};
+
+document.addEventListener ('scroll', onScroll);
+nav.addEventListener ('mouseover', hoverOn);
+nav.addEventListener('mouseout', hoverOff);
